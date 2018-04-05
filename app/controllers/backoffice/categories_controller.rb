ad7fc6d896a994f529ce8726acfb696f80a2f299
Matheus <mtesedd@hotmail.com>
2018-04-05 12:18:01 -0300

    Config categories controller and routes

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -2,7 +2,24 @@ class Backoffice::CategoriesController < ApplicationController
   before_action :authenticate_admin!
 
   layout "backoffice"
-  
+
   def index
+    @categories = Category.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+  
+  def edit
+
+  end
+
+  def update
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
+
+  get 'backoffice', to: 'backoffice/dashboard#index'
   namespace :backoffice do
-    get 'categories/index'
+    resources :category, except: [:show, :destroy]
+    get 'dashboard', to: 'dashboard#index'
+    get 'categories', to: 'categories#index'
   end
   namespace :site do
     get 'home', to: 'home#index'
   end
 
-  get 'backoffice', to: 'backoffice/dashboard#index'
+
 
   namespace :backoffice do
-    get 'dashboard', to: 'dashboard#index'
+
   end
   devise_for :admins
   devise_for :users
