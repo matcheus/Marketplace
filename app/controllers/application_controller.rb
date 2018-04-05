9eac7483e30381c2aac4007c94d54c58c43fae0e
Matheus <mtesedd@hotmail.com>
2018-04-05 11:19:17 -0300

    Devise layout config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  # Layout per resource_name
+  def layout_by_resource
+    if devise_controller? && resource_name == :admin
+      "devise_backoffice"
+    else
+      "application"
+    end
+  end
 end
